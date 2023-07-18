8455cf5ba4036a0bdcf509aa6dcc92794afcd1cd
Jonatan Branting <jonatan.branting@gmail.com>
2023-07-18 19:19:44 +0200

    Add `lib.key`
    A convenience module for defining mappings.

---
+++ lua/lib/key.lua
@@ -0,0 +1,85 @@
+-- This module enables the following syntax to be used for mapping keys:
+-- ```
+-- Key.n.desc("Save file").set("<leader>s", ":w<cr>")
+
+-- Group.n
+-- .desc("Alternate mappings")
+-- .set("<leader>v", function(group)
+--   group.set("r", ":luafile %<cr>", "Reload config")
+-- end)
+-- ```
+
+local Key = {}
+
+-- This allows us to use `Key.n` instead of `Key:new().n`
+setmetatable(Key, {
+  __index = function(_, k)
+    -- TODO: This stack-overflows when we try to access a key that
+    -- doesn't exist
+    return Key:new()[k]
+  end,
+})
+
+function Key:new()
+  local instance = {
+    modes = {},
+    flags = {},
+    _group = "",
+  }
+
+  setmetatable(instance, self)
+
+  -- This allows us to chain modes like this : `Key.n.o.x`
+  self.__index = function(_, k)
+    local possible_modes = {
+      n = "n",
+      i = "i",
+      v = "v",
+      x = "x",
+      o = "o",
+      t = "t",
+      c = "c",
+      s = "s",
+      l = "l",
+      ["!"] = "!",
+    }
+
+    local possible_flags = {
+      silent = { silent = true },
+      expr = { expr = true },
+      nowait = { nowait = true },
+    }
+
+    if possible_modes[k] ~= nil then
+      table.insert(instance.modes, possible_modes[k])
+      return instance
+    elseif possible_flags[k] ~= nil then
+      instance.flags = vim.tbl_extend("force", instance.flags, possible_flags[k])
+      return instance
+    else
+      return self[k]
+    end
+  end
+
+  return instance
+end
+
+function Key:__call(...)
+  return self.set(...)
+end
+
+function Key:group(desc, group)
+  -- TODO: Add a way to set the group description, possibly this is which-key specific
+  self._group = group
+
+  return self
+end
+
+function Key:set(desc, key, value)
+  local opts = vim.tbl_extend("force", self.flags, { desc = desc })
+  vim.keymap.set(self.modes, self._group .. key, value, opts)
+
+  return self
+end
+
+return Key
