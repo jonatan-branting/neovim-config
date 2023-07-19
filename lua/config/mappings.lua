e56dc9432b66c62ea9ade75cd97d0b47e84139b1
Jonatan Branting <jonatan.branting@gmail.com>
2023-07-19 15:34:20 +0200

    Remove old `nnoremap`-style mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,33 +1,6 @@
 local utils = require("lib.utils")
 local Enumeratable = require("lib.enumeratable")
 local Key = require("lib.key")
-local keymap = vim.keymap
-
-local function nnoremap(opts, desc)
-  return keymap.set("n", unpack(opts or {}))
-end
-
-local function inoremap(opts, desc)
-  return keymap.set("i", unpack(opts or {}))
-end
-
-local function snoremap(opts, desc)
-  return keymap.set("s", unpack(opts or {}))
-end
-
-local function cnoremap(opts, desc)
-  return keymap.set("c", unpack(opts or {}))
-end
-
-local function vnoremap(opts, desc)
-  return keymap.set("x", unpack(opts or {}))
-end
-
--- Convenience <leader> mappings
-Key.n.silent
-  :group("Windows", "<leader>w")
-  :set("Right window", "l", "<c-w>l")
-  :set("Left window", "h", "<c-w>h")
 
 -- General opinionated behaviour changes
 Key.i.s.c:set("Escape", "jj", "<esc>")
@@ -74,6 +47,8 @@ Key.v:set("Search and replace", "<c-r>", ":%s///gc<left><left><left>")
 -- Consistency please!
 Key.n:set("Select line", "vv", "0v$")
 Key.n:set("Yank to end of line", "Y", "y$")
+Key.n:set("Visual to end of line", "V", "v$")
+Key.n:set("Visual entire line", "vv", "V")
 
 -- Windows
 Key.n.silent
@@ -114,10 +89,6 @@ Key.n:set("Reset current file", "<leader>gr", function()
   print("Checked out " .. vim.fn.expand("%") .. " from " .. default_branch)
 end)
 
--- Easy Align
-vnoremap({ "<cr>", "<Plug>(EasyAlign)" }, "easy-align-selected")
-nnoremap({ "ga", "<Plug>(EasyAlign)" }, "easy-align")
-
 -- Asterisk
 Key.n
   :set("Search word under cursor", "*", "<Plug>(asterisk-*)")
@@ -136,56 +107,46 @@ Key.n
 -- Allow line split using S, as opposed to J(oin)
 -- vim.keymap.set("n", 'S', 'i<cr><Esc>^mwgk:silent! s/\v +$//<cr>:noh<cr>')
 
--- I'm not using this, and I want a free key
-nnoremap({ "t", "<nop>" })
-
 -- Nearly same as <cr>
-nnoremap({ "_", "<nop>" })
+Key.n:set("Unmap _", "_", "<nop>")
 
--- Begone
-nnoremap({ "Q", "<nop>" })
+-- This is horrible
+Key.n:set("Unmap Q", "Q", "<nop>")
 
-local todo = require("modules.todo")
+-- TODO can we treat modules as local plugins and make this part of a
+-- lazy spec instead?
+Key.n:set("Open branch todo", "<leader>z", function()
+  require("modules.todo").open_branch_todo()
+end)
 
--- TODO come up with good context prefix <leader>z is too short
--- nnoremap({ "<leader>z", todo.open_branch_todo }, "open-branch-todo")
+Key.n:set("Alternate last buffer", "<leader>a", "<c-^>")
 
-nnoremap({ "<c-k>", ":m .-2<CR>==" })
-nnoremap({ "<c-j>", ":m .+1<CR>==" })
-vnoremap({ "<c-k>", ":m '<-2<CR>gv=gv" })
-vnoremap({ "<c-j>", ":m '>+1<CR>gv=gv" })
+Key.n:set("Move line up", "<c-k>", ":m .-2<CR>=="):set("Move line down", "<c-j>", ":m .+1<CR>==")
 
-vnoremap({ "<up>", ":m '<-2<CR>gv=gv" })
-vnoremap({ "<down>", ":m '>+1<CR>gv=gv" })
-vnoremap({ "<left>", "<gv" })
-vnoremap({ "<right>", ">gv" })
+Key.v
+  :set("Move line up", "<c-k>", ":m '<-2<CR>gv=gv")
+  :set("Move line down", "<c-j>", ":m '>+1<CR>gv=gv")
 
-nnoremap({ "<down>", "<c-w>j" })
-nnoremap({ "<up>", "<c-w>k" })
-nnoremap({ "<left>", "<c-w>h" })
-nnoremap({ "<right>", "<c-w>l" })
+Key.v
+  :set("Move line up", "<up>", ":m '<-2<CR>gv=gv")
+  :set("Move line down", "<down>", ":m '>+1<CR>gv=gv")
+  :set("Deindent line", "<left>", "<gv")
+  :set("Indent line", "<right>", ">gv")
 
-nnoremap({ "<s-down>", require("smart-splits").resize_down })
-nnoremap({ "<s-up>", require("smart-splits").resize_up })
-nnoremap({ "<s-left>", require("smart-splits").resize_left })
-nnoremap({ "<s-right>", require("smart-splits").resize_right })
+Key.n
+  :set("Window left", "<c-h>", "<c-w>h")
+  :set("Window down", "<c-j>", "<c-w>j")
+  :set("Window up", "<c-k>", "<c-w>k")
+  :set("Window right", "<c-l>", "<c-w>l")
 
-nnoremap({ "<c-a>", "0" })
-nnoremap({ "<c-e>", "$" })
+-- readline like keybinds
+Key.n:set("Start of line", "<c-a>", "0")
+Key.n:set("End of line", "<c-e>", "$")
 
-nnoremap({ "<leader>a", "<c-^>" }, "alternate-last-buffer")
-
-nnoremap({ "yc", "<c-w>c" }, "close-window")
-
-inoremap({ ";;", "<esc>A;<esc>" })
-inoremap({ ",,", "<esc>A,<esc>" })
-
+-- TODO this should just be part of the context menu!
 vim.keymap.set("n", "<leader>vr", function()
   return ":IncRename " .. vim.fn.expand("<cword>")
 end, { expr = true })
-
-cnoremap({ "<c-x>", "<c-v><esc>" })
-vnoremap({ "<tab>", '"zc' })
 
 vim.keymap.set("x", "p", "pgvy=']", {})
 vim.keymap.set({ "x", "n" }, "p", "p=']", {})
@@ -212,7 +173,6 @@ vim.keymap.set("n", "X", "ddO<esc>")
 
 -- searches for the last text that was changed, and replaces it with the changes made
 -- is dot repeatable
-
 _G.search_and_replace_last_wrapper = function()
   vim.go.operatorfunc = "v:lua.search_and_replace_last_callback"
 
@@ -246,21 +206,12 @@ vim.keymap.set("n", "<leader-b>", function()
   vim.cmd([[ set hls ]])
 end)
 
-vim.keymap.set("n", "V", "v$")
-vim.keymap.set("n", "vv", "V")
-
--- Populate jumplist on insert mode!
-vim.keymap.set("n", "i", "m'i")
-
--- vim.keymap.set("n", "J", "Jx")
 vim.keymap.set("x", "/", "<Esc>/\\%V")
-vim.keymap.set({ "n", "x" }, "<cr>", ":")
-vim.keymap.set({ "n", "x" }, "<s-cr>", ":%")
 vim.keymap.set({ "n" }, "x", "/")
 vim.keymap.set({ "x" }, "x", "<esc>/\\%V")
 
--- join but remove whitespace
-vim.keymap.set("n", "J", function()
+-- Join but remove whitespace
+Key.n:set("Join lines removing whitespace", "J", function()
   vim.cmd("normal! mzJ")
 
   local col = vim.fn.col(".")
@@ -277,51 +228,30 @@ vim.keymap.set("n", "J", function()
     vim.cmd("undojoin | normal! hx")
   end
 
+  vim.cmd("normal! m'") -- Populate jumplist
   vim.cmd("normal! `z")
 end)
 
-vim.keymap.set("n", "dd", function()
-  if vim.api.nvim_get_current_line():match("^%s*$") then
-    return '"_dd'
-  else
-    return "dd"
-  end
-end, {
-  expr = true,
-})
+Key.n.expr
+  :set("Indent aware delete", "dd", function()
+    if vim.api.nvim_get_current_line():match("^%s*$") then
+      return '"_dd'
+    else
+      return "dd"
+    end
+  end)
+  :set("Indent aware insert", "i", function()
+    if #vim.fn.getline(".") == 0 then
+      return [["_ci]]
+    else
+      return "i"
+    end
+  end)
 
-vim.keymap.set("n", "i", function()
-  if #vim.fn.getline(".") == 0 then
-    return [["_cc]]
-  else
-    return "i"
-  end
-end, {
-  expr = true,
-})
-
-vim.keymap.set("n", "<leader>l", function()
-  require("modules.term"):get_terminal():toggle()
-end)
-vim.keymap.set({ "n", "x" }, "<leader>x", function()
-  -- TODO we can likely expand this to language specific mappings, which might wrap things
-  -- to auto-monkey-patch stuff in Ruby, for example, by using Treesitter
-
-  require("modules.term"):get_terminal():send(vim.fn.getline("."))
-end)
-vim.keymap.set("n", "<leader>l", function()
-  require("modules.term"):get_terminal():toggle()
-end)
-
--- vim.keymap.set('n', 'o', function()
---   local line = vim.api.nvim_get_current_line()
-
---   local should_add_comma = string.find(line, '[^,{[]$')
---   if should_add_comma then
---     return 'A,<cr>'
---   else
---     return 'o'
---   end
--- end, { buffer = true, expr = true })
-
--- TODO also make f go cross lines
+Key.n
+  :set("Send line to terminal", "<leader>x", function()
+    require("modules.term"):get_terminal():send(vim.fn.getline("."))
+  end)
+  :set("Toggle terminal", "<leader>l", function()
+    require("modules.term"):get_terminal():toggle()
+  end)
